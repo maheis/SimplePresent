4355898b8a60ffe60d00e3332034862e8ae2884f
maheis <manfred@heister.email>
2026-08-25 21:54:17 +0200

    fix: add missing import for Bundle in MainActivity

--- android/app/src/main/kotlin/de/maheis/simplepresent/MainActivity.kt
+++ android/app/src/main/kotlin/de/maheis/simplepresent/MainActivity.kt
@@ -6,6 +6,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import android.Manifest
 import android.app.AlarmManager
 import org.json.JSONArray
